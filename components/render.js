51882c18d45b1ac6c89a332ada7e4dcec74f422b
Ilmira260696 <zackirovaelmira26@mail.ru>
2023-08-04 11:57:43 +0400

    сделала верстку 2 и 3 стр

--- components/render.js
+++ components/render.js
@@ -1,8 +1,17 @@
-// import { cards } from './cards.js'
+import { cards } from './cards.js'
+
 export function renderGame(level, appEl) {
+    let levelGame = level.value
+    const backSide = []
+    for (let i = 0; i < levelGame; i++) {
+        backSide.push(`<img class='card-game-back' src='./img/рубашка.png' alt='img'>
+        `)
+    }
+    console.log(backSide)
+
     const appHtml = `
-<div class="title">
-<header> 
+<div class="card-game">
+<header class="card-game-timer"> 
     <div class="top">
     <div class="duration-time">   
        <p class="duration">min</p>
@@ -13,11 +22,19 @@ export function renderGame(level, appEl) {
 
        <button class="button-two" id="submit-button" type="submit" >Начать заново</button>
 </header>
-
-       
-</div> 
-`
+<div class="approach center" id="approach">${backSide.join('')}</div>      
+</div> `
     appEl.innerHTML = appHtml
+    const reverseCards = document.querySelectorAll('.card-game-back')
+    for (const reverseCard of reverseCards) {
+        reverseCard.addEventListener('click', () => {
+            let sortCardArray = cards.sort(() => Math.random()- 0.5).slice(0, levelGame/2)
+            sortCardArray = sortCardArray.concat(sortCardArray).sort(() => Math.random() - 0.5).join('')
+            const approach = document.getElementById('approach')
+            approach.innerHTML = `${sortCardArray}`
+            setTimeout(() => {
+                renderGame(level, appEl)
+            }, 5000)
+        })
+    }
 }
-
-
--- index.js
+++ index.js
@@ -9,13 +9,13 @@ function gameComplexity() {
     <h1 class="top-content-title"> Выбери <br>сложность</h1> 
    
 <div class="radio-toolbar"> 
-    <input class="radio-toolbar-radio" type="radio" id="radio1" name="radios" value="1">
+    <input class="radio-toolbar-radio" type="radio" id="radio1" name="radios" value="6">
     <label class="radio-toolbar-label" for="radio1" value="1">1</label>
     
-    <input class="radio-toolbar-radio" type="radio" id="radio2" name="radios" value="2">
+    <input class="radio-toolbar-radio" type="radio" id="radio2" name="radios" value="12">
     <label class="radio-toolbar-label" for="radio2" value="2">2</label>
     
-    <input class="radio-toolbar-radio" type="radio" id="radio3" name="radios" value="3">
+    <input class="radio-toolbar-radio" type="radio" id="radio3" name="radios" value="18">
     <label class="radio-toolbar-label" for="radio3" value="3">3</label>
 </div> 
     <button class="form-button" id="submit-button" type="submit">Старт</button>
